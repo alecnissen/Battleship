c84c5d99d51f6e10d3af141ff3b009f47bd90ed3
alecnissen <alecnissen@yahoo.com>
2023-07-12 14:02:56 -0400

    added a second unit test which checks if the ship has been sunk or not based upon the hit counter, currently it is returning the correct value if hitCounter is greater than or equal to the ships length

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -9,3 +9,12 @@ test('testing to see if hit incrementor function is incrementing and getHitIncre
     expect(battleShip.getHitIncrementor()).toBe(2);
 }) 
 
+test('testing to see if the isSunkConditional and getShipStatus functions are returning the correct status, isSunk within getShipStatus should return true if hitCounter >= length', () => {
+    let battleShip = ship('Battleship', 4);
+    battleShip.hitIncrementor();
+    battleShip.hitIncrementor();
+    battleShip.hitIncrementor();
+    battleShip.hitIncrementor();
+    battleShip.isSunkConditional();
+    expect(battleShip.getShipStatus()).toBe(true);
+})
