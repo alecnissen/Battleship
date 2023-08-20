ab0ca93ddb995a69df85a8c5f94e4b7d4c4d89ec
alecnissen <alecnissen@yahoo.com>
2023-08-20 16:58:41 -0400

    refactored turn switching logic within game module, also was able to generate random coordinates for when computer attacks players board. As well as checkForWinner function, next will add game loop conditonal, to make sure the game keeps playing as long as the condition is true

--- src/gameModule.js
+++ src/gameModule.js
@@ -2,64 +2,170 @@ import ship from './shipFactory.js';
 import gameboardFactory from './gameboardFactory.js'; 
 import playerFactory from './playerFactory.js';
 
-// before determining hits, or turn switching logic, 
-// create two users from factory 
-// place ships on the players board, create the method
-// computers board will be randomly generated 
-
-
-// At this point it is appropriate to begin crafting your User Interface.
-// The game loop should set up a new game by creating Players and Gameboards. 
-// For now just populate each Gameboard with predetermined coordinates. You can implement a system for allowing players to place their ships later.
-
-// const player = playerFactory('player');
-// const computer = playerFactory('computer');
-
-// console.log(player);
-// console.log(computer);
-
-// The game loop should set up a new game by creating Players and Gameboards. 
-// For now just populate each Gameboard with predetermined coordinates. You can implement a system for allowing players to place their ships later.
-
-// I made a method which returns players name, it already has the board in the variable, could I just start populating the board 
-
-// you already have the method which creates players, 
-
-// refactor these functions to populate the board, 
-
-// populate board, 
-
-// hit board 
-
-// determine turn, logic of switching turns, 
-
-// determine if ships are sunk
-
-// not sure what I need to test, 
-
-// test that create player returns the correct value? 
-
-// 
-
-
+// create players here
 function createPlayer(name, type) { 
     if(type === 'computer') {
-      const computerName = playerFactory(name);
+      const computerName = playerFactory(name); // add turn var
       return computerName;
     } else {
-      const playerName = playerFactory(name);
+      const playerName = playerFactory(name);   // add turn var
       return playerName;
+    }
+  } 
+  
+  function placeShipsOnPlayersBoard(user) { 
+    const player = user; 
+    const battleShip = ship('Battleship', 4, 'vertical');
+    const destroyer = ship('Destroyer', 4, 'horizontal' );
+    const patrolBoat = ship('Patrol-boat', 2, 'vertical');
+    const carrierBoat = ship('Carrier', 4, 'horizontal');
+    const submarine = ship('Submarine', 3, 'vertical');
+    // const player = createPlayer('player', 'player');
+    // console.log(player);
+    const getPlayerBoard = player.gameboard.getGameboard();
+    const placeBattleShip = player.gameboard.placeShip(battleShip, 0, 0, 4, 'horizontal');
+    const placeDestroyer = player.gameboard.placeShip(destroyer, 1, 1, 4, 'horizontal');
+    const placePatrolBoat = player.gameboard.placeShip(patrolBoat, 2, 2, 2, 'horizontal');
+    const placeCarrierBoat = player.gameboard.placeShip(carrierBoat, 3, 3, 4, 'horizontal');
+    const placeSubmarine = player.gameboard.placeShip(submarine, 4, 4, 3, 'horizontal');
+    console.log(getPlayerBoard);
+    return getPlayerBoard;
+  } 
+
+  function placeShipsOnComputersBoard(user) { 
+    const computer = user;
+    const battleShip = ship('Battleship', 4, 'vertical');
+    const destroyer = ship('Destroyer', 4, 'horizontal' );
+    const patrolBoat = ship('Patrol-boat', 2, 'vertical');
+    const carrierBoat = ship('Carrier', 4, 'horizontal');
+    const submarine = ship('Submarine', 3, 'vertical');
+    // const computer = createPlayer('PC', 'computer');
+    const getComputerBoard = computer.gameboard.getGameboard();
+    const placeBattleShip = computer.gameboard.placeShip(battleShip, 0, 0, 4, 'horizontal');
+    const placeDestroyer = computer.gameboard.placeShip(destroyer, 1, 1, 4, 'horizontal');
+    const placePatrolBoat = computer.gameboard.placeShip(patrolBoat, 2, 2, 2, 'horizontal');
+    const placeCarrierBoat = computer.gameboard.placeShip(carrierBoat, 3, 3, 4, 'horizontal');
+    const placeSubmarine = computer.gameboard.placeShip(submarine, 7, 4, 3, 'horizontal');
+    console.log(getComputerBoard);
+    return getComputerBoard;
+
+  }
+
+  // think of the game flow, 
+  // players have their ships on the board, 
+  // now we can determine whose turn it is, 
+  // once we know whose turn it is, that user can make there hit 
+  // once the user makes hit, 
+  // check if all ships sunk, if no, keep playing 
+  // switch to the other user, user makes hit, check if all ships sunk, if no, switch to other user, 
+
+  function playGame() { 
+    let playerTurn = 1;
+    const computer = createPlayer('PC', 'computer') // added turn var
+    const player = createPlayer('player', 'player') // add turn var
+
+    console.log(computer);
+    console.log(player);
+
+    while (!computer.gameboard.areAllShipsSunk() && player.gameboard.areAllShipsSunk()) { 
+      // all the code goes in here, within the condition.
+    }
+
+    // place ships on players board
+    placeShipsOnPlayersBoard(player);
+    // place ships on computers board 
+    placeShipsOnComputersBoard(computer); 
+
+    // while areAllShipsSunk() is false for both gameboards, keep playing,
+
+    // determine whose turn it is, let player go first, 
+    // once we figure it out, pass the enemy's board, and pass in the players attack coordinates (x, y)
+    // call attack, pass in enemys board and the users attack coordinates, use receiveAttack on enemys board. 
+    
+    // player has gone, disable them to prevent them for making a turn, 
+    //  allow computer to follow same process. Then once computer goes, disable computer and allow player to go. 
+
+    // attack(userObj, x, y)
+    if (playerTurn === 1) { // player is === 1, attack comps board
+      attack(computer, 3, 3); // can test for multiple attacks, apply attack method x amount of times, 
+      if (checkForWinner(computer)) { 
+        return; // print/access winner modal
+      }
+      playerTurn = 2;
+
+    } else { // have to make the computer capable of making random moves, generate random coordinates, 
+      // call another function here, which generates 2 random numbers, and returns them, 
+      const randomCoordinate1 = Math.floor(Math.random() * 9) + 1;
+      const randomCoordinate2 = Math.floor(Math.random() * 9) + 1;
+      attack(player, randomCoordinate1, randomCoordinate2); // this should be random attack, 
+      if (checkForWinner(player)) { 
+        return; // print/access winner modal
+      } 
+      playerTurn = 1;
     }
   }  
 
+  function checkForWinner(userObj) { 
+    let selectedUser = userObj;
+    if (selectedUser.gameboard.areAllShipsSunk()) { 
+      return true;
+    } 
+    return false;
+  }
 
-  function playGame() { 
-    const computer = createPlayer('PC', 'computer')
-    const player = createPlayer('player', 'player')
-   // rest of the function
-  }  
 
-console.log(createPlayer('Alec', 'player')); 
+  function attack(userObj, x, y) { 
+    let selectedUser = userObj;
+    selectedUser.gameboard.receiveAttack(x, y);
+  }
+
+// let randomCoordinate1 = Math.floor(Math.random() * 9) + 1;
+// let randomCoordinate2 = Math.floor(Math.random() * 9) + 1;
+
+// console.log(randomCoordinate1);
+// console.log(randomCoordinate2);
+
+
+  playGame();
+
+  // old attack function
+  // function attack(user1, user2,) { 
+  //   let computer = user1;
+  //   let player = user2;
+
+  //   console.log(computer);
+  //   console.log('computers userTurn value before attack',computer.userTurn);
+  //   console.log('players userTurn value before attack', player.userTurn);
+
+  //   if (player.userTurn === true) { 
+  //     // if true let player make attack, on computers gameboard
+  //     console.log(computer.gameboard.receiveAttack(3, 3));
+  //     player.userTurn = false;
+  //     console.log('players userTurn value after attack', player.userTurn)
+  //     computer.userTurn = true;
+  //     console.log('computers userTurn value after attack', computer.userTurn)
+  //   } 
+
+  //   console.log('players userTurn value attack, outside if block', player.userTurn);
+
+  //   if (computer.userTurn === true) { 
+  //     console.log(player.gameboard.receiveAttack(1, 2));
+  //     computer.userTurn = false;
+  //     console.log('computers userTurn value after attack', computer.userTurn);
+  //     player.userTurn = true;
+  //     console.log('players userTurn value after comp attack', player.userTurn);
+  //   } 
+  // }
+
+// console.log(createPlayer('Alec', 'player')); 
+
+// let testPlayer = createPlayer('Alec', 'player');
+
+
+
+// console.log(placeShipsOnPlayersBoard(testPlayer));
+
+
 
 
 
